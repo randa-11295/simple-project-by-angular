ee7691c5f223a67fb0f5e4b43e1727bd66d3ba09
randa-11295 <randa.mohamed1295@gmail.com>
2022-06-26 13:01:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DuctorsComponent } from './compomians/ductors/ductors.component';
 import { RatingComponent } from './compomians/rating/rating.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsComponent } from './forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DuctorsComponent,
     RatingComponent,
-    FormsComponent,
     HomeComponent,
-    HeaderComponent,
-    
+     
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     NgbModule, 
+    FormsModule,
      BrowserModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomeComponent },
-      {path: 'sign', component:  FormsComponent },
+      {path: '', component: HomeComponent },
    
     ]),
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title = 'docturs';
   list: any = [
-    { name: 'mohamed ahmed', job: 'opthalmomgist', location: "portsaid", price: 200, phone: "012345678901", img: "1.jpg" },
-    { name: 'randa mohamed', job: 'techar', location: "alex", price: 400, phone: "789002736278", img: "2.jpg" },
-    { name: 'fady salah', job: 'dintast', location: "cario", price: 50, phone: "122121123", img: "3.jpeg" },
-    { name: 'aya magdy', job: 'opthalmomgist', location: "mansura", price: 120, phone: "6828028202", img: "4.png" },
-    { name: 'temon and bomba', job: 'mental', location: "disny", price: 2000, phone: "68192700", img: "5.jpg" },
-
+    { name: 'mohamed', job: 'opthalmomgist', location: "portsaid", price: 200, phone: "012345678901", img: "1.jpg" },
+    { name: 'randa', job: 'techar', location: "alex", price: 400, phone: "789002736278", img: "2.jpg" },
+    { name: 'fady', job: 'dintast', location: "cario", price: 50, phone: "122121123", img: "3.jpeg" },
+    { name: 'aya', job: 'opthalmomgist', location: "mansura", price: 120, phone: "6828028202", img: "4.png" },
+    { name: 'temon ', job: 'mental', location: "disny", price: 2000, phone: "68192700", img: "5.jpg" },
   ]
+
+
+  newList = this.list 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  data:any = ""
+
+  changeFn(): void {
+    console.log(this.data) 
+    this.newList = this.list.filter((el:any)=>{
+        console.log(el.name) 
+      return el.name === this.data }) 
+    console.log(this.data)
+  }
 }
+
+
